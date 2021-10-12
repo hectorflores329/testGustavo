f4b04d4de6aadfe1333500a0187a6db388b14f23
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-10-12 12:08:27 -0600

    Commit

--- servidor.py
+++ servidor.py
@@ -6,10 +6,8 @@ def general():
     descarga()
 
 def descarga():
-    url = "https://regiones.ine.cl/documentos/default-source/region-xv/estadisticas/generacion-y-distribucion-de-energia-electrica/cuadros-estadisticos/series-mensuales/series-mensuales-desde-2015-a-la-fecha.xlsx?sfvrsn=c19505b2_8"
-    file = requests.get(url)
-
-    open('text.xlsx', 'wb').write(file.content)
+    # LÓGICA COMPLETA
+    print("HOLA")
 
 
 if __name__ == '__main__':
